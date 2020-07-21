5f8307b22f34d1234661735e1733d1a68e414d6a
linkjavier <linkjavier@hotmail.com>
2020-07-21 00:33:40 -0500

    Added newnode->right = NULL

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -21,6 +21,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 	newnode->parent = parent;
 	newnode->n = value;
+	newnode->right = NULL;
 	newnode->left = parent->left;
 	if (newnode->left != NULL)
 		newnode->left->parent = newnode;
