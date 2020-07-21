c6a0e6fcfa2038f39142ec16768f193b7acc7387
mfcrespo <mafe_crespo@hotmail.com>
2020-07-21 02:16:03 +0000

    Include print function

--- binary_trees.h
+++ binary_trees.h
@@ -13,11 +13,13 @@
  */
 struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 };
+
+typedef struct binary_tree_s binary_tree_t;
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
@@ -38,10 +40,6 @@ int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
-
-typedef struct binary_tree_s binary_tree_t;
-typedef struct binary_tree_s bst_t;
-typedef struct binary_tree_s avl_t;
-typedef struct binary_tree_s heap_t;
+void binary_tree_print(const binary_tree_t *tree);
 
 #endif /*_BINARY_TREES_H_*/
