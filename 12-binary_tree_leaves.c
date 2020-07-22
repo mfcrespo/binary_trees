77fb92fe4911bb387c3c148f5f86858f131a4c65
linkjavier <linkjavier@hotmail.com>
2020-07-22 15:52:52 -0500

    Fixed Betty in  12-binary_tree_leaves.c

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -21,7 +21,7 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 			leaves += 1;
 
 		leaves += binary_tree_leaves(tree->left);
-        leaves += binary_tree_leaves(tree->right);
+		leaves += binary_tree_leaves(tree->right);
 	}
 	return (leaves);
 }
